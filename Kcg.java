db21bae94dca533bf2698a561c4a73658c68065d
ko1aru <83403149+ko1aru@users.noreply.github.com>
2021-09-09 15:07:02 +0530

    Add files via upload

---
+++ Kcg.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Kcg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int s=95;
+		char a='s';
+		a=(char)s;
+		System.out.println(a);
+		
+	}
+
+}
