019bb34e3eb5ee29f5f4cee6c9ca94051de8570d
ko1aru <83403149+ko1aru@users.noreply.github.com>
2021-09-11 12:20:33 +0530

    Update Kcg.java

--- Kcg.java
+++ Kcg.java
@@ -3,12 +3,10 @@ package test;
 public class Kcg {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		int s=95;
 		char a='s';
 		a=(char)s;
-		System.out.println(a);
-		
+		System.out.println(a);		
 	}
-
 }
