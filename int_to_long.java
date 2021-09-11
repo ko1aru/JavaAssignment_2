e4dd45f2cf21bdd6fda848926d146645afd4b565
ko1aru <83403149+ko1aru@users.noreply.github.com>
2021-09-11 12:20:00 +0530

    Update int_to_long.java

--- int_to_long.java
+++ int_to_long.java
@@ -2,14 +2,16 @@ import java.util.Scanner;
 public class int_to_long {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		int intnum;
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter a integer number: "+"\n");
 		intnum = sc.nextInt();
+		//int to long
 		long longnum = intnum;
 		System.out.println("After converting to long: "+longnum);
 		System.out.println("Datatype: "+((Object)longnum).getClass().getSimpleName()+"\n");
+		//long to float
 		float floatnum=longnum;
 		System.out.println("After converting to float: "+floatnum);
 		System.out.println("Datatype: "+((Object)floatnum).getClass().getSimpleName());
